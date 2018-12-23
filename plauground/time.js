c03608ab585a2cad7fe76528a570cde330fcf0a3
Sinisha Mijajlovikj <sinis.bt@gmail.com>
2018-12-23 23:58:08 +0100

    format timstamps using moments js

---
+++ plauground/time.js
@@ -0,0 +1,12 @@
+//jan 1st 1970 00:00:00 am  -- unix epic
+
+var moment  = require('moment');
+
+var date = moment();
+
+
+console.log( date.format('MMM YYYY') );
+
+
+
+
--- public/js/index.js
+++ public/js/index.js
@@ -8,20 +8,21 @@ socket.on('disconnect', function() {
     console.log('disconnected from sever');
 });
 
-socket.on('newMessage', function(message){
-    console.log('newMessage', message);
+socket.on('newMessage', function(message){    
+    var formatedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formatedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 
 });
 
 socket.on('newLocationMessage', function(message){
+    var formatedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My location</a>');
+    var a = jQuery('<a target="_blank">My location</a>');    
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formatedTime}: `);
     a.attr('href', message.url);
     li.append(a);
 
